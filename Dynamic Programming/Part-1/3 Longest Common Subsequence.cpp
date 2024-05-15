092eaa3133a6da6d8cb3215d610af84cbea08a95
unknown <vinayaknikam2001@gmail.com>
2024-05-15 08:22:23 +0530

    Revising DP 15May2024

--- Dynamic Programming/Part-1/3 Longest Common Subsequence.cpp
+++ Dynamic Programming/Part-1/3 Longest Common Subsequence.cpp
@@ -22,3 +22,39 @@ int lcs(string s, string t)
     vector<vector<int>> dp(n1,vector<int>(n2, INT_MIN));
 	return func(n1-1, n2-1, s, t, dp);
 }
+
+
+//LeetCode = https://leetcode.com/problems/longest-common-subsequence/description/
+class Solution {
+public:
+
+    int findLCS(int i, int j, string& s1, string &s2, vector<vector<int>> &dp)
+    {
+        if (i >= s1.size() || j >= s2.size())
+        {
+            return 0;
+        }
+
+        if (dp[i][j] != INT_MIN)
+        {
+            return dp[i][j];
+        }
+
+        int iPik = 0, iNPik = 0;
+        if (s1[i] == s2[j])
+        {
+            iPik = 1 + findLCS(i+1, j+1, s1, s2, dp);
+        }
+        iNPik = 0 + max(findLCS(i+1, j, s1, s2, dp), findLCS(i, j+1, s1, s2, dp));
+
+        return dp[i][j] = max(iPik, iNPik);
+    }
+
+    int longestCommonSubsequence(string text1, string text2) 
+    {
+        int n1 = text1.size();
+        int n2 = text2.size();
+        vector<vector<int>> dp(n1, vector<int>(n2, INT_MIN));
+        return findLCS(0, 0, text1, text2, dp);
+    }
+};
