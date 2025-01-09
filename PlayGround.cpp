fa0f6c615ad6c969b58df47a7d01a4fc8d427b24
Vinayak Nikam <vinayaknikam2001@gmail.com>
2025-01-09 21:54:14 +0530

    Revised Future & Promise

--- PlayGround.cpp
+++ PlayGround.cpp
@@ -3,30 +3,45 @@ using namespace std;
 typedef long long int lli;
 typedef unsigned long long ull;
 
-
-class Singleton
-{
-private:
-
-    Singleton(){}
-    ~Singleton(){}
+class Solution {
 public:
-    Singleton(const Singleton &objSingleton) = delete;
-    Singleton operator=(const Singleton &objSingleton) = delete;
-
-    static Singleton& getInstance() 
+    int minimumOperations(vector<vector<int>>& grid) 
     {
-        static Singleton objSingleton;
-        return objSingleton;
+        int iRes = 0, iRow = 0, iCol = 0;
+        iRow = grid.size ();
+        iCol = grid[0].size();
+        
+        for (int c=0; c<iCol; ++c)
+        {
+            int iPrev = grid[0][c];
+            for (int r=1; r<iRow; ++r)
+            {
+                if (grid[r][c] <= iPrev)
+                {
+                    int iVal = (iPrev + 1 - grid[r][c]);
+                    iRes += iVal;
+                    iPrev = grid[r][c] + iVal;
+                }
+                else {
+                    iPrev = grid[r][c];
+                }
+            }
+        }
+        
+        
+        return iRes;
     }
-
 };
 
-
-    
-int main()
+int main () 
 {
-    Singleton &obj = Singleton::getInstance();
-    cout<<"Success!";
+
+    Solution obj;
+    vector<vector<int>> vec = 
+    {
+        {3,2},{1,3},{3,4},{0,1}
+    };
+    cout<<obj.minimumOperations(vec);
+    
     return 0;
 }
--- Threads/FuturePromise.cpp
+++ Threads/FuturePromise.cpp
@@ -15,12 +15,15 @@ void getSum(std::promise<int>&& promSum, int iVal)
     {
         iSum += i;
     }
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     promSum.set_value(iSum);
+    cout<<"\nPromise Sets value";
     /*
     Here thread might have other tasks to do but once promise.set_value() is called futSum.get()
-    will resume it tasks even this threads has further tasks to do :)
+    will resume (Untill that futSum.get() thread will be blocked) it tasks even this threads has further tasks to do :)
     */
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    cout<<"\nProceeding further task!";
 }
 
 //This program created to calculate sum of all numbers using promise thread sync.
@@ -31,9 +34,9 @@ int main()
     std::future<int> futSum = promSum.get_future();
 
     std::thread t1(getSum, std::move(promSum), iVal);
-
-    cout<<"Calculating the sum...\n";
-    cout<<"Sum is :"<<futSum.get(); // Here
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    cout<<"\nCalculating the sum...";
+    cout<<"\nSum is :"<<futSum.get(); // Here
 
     t1.join();
 
