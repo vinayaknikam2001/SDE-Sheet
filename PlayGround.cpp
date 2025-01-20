4092dde3c0689d13759e654eef154a52ee2a570f
Vinayak Nikam <vinayaknikam2001@gmail.com>
2025-01-20 08:07:26 +0530

    Made Changes in Practice File Revising DP

--- PlayGround.cpp
+++ PlayGround.cpp
@@ -4,10 +4,28 @@ typedef long long int lli;
 typedef unsigned long long ull;
     
     
-int main()
+long maxPossibleWays(int *den, int i, int iVal, int iSum = 0)
 {
-    int a[3] = {1,2,3};
-    for (auto i:a)
-        cout<<" "<<i;
-    return 0;
+    if (0 == iVal)
+    {
+        return 1;
+    }
+    if (0 == i)
+    {
+        return  (0 == iVal%den[i]) ? iVal/den[i] : 0;
+    }
+
+    long iPik = 0, iNPik = 0;
+    if (den[i] <= iVal)
+    {
+        iPik = maxPossibleWays(den, i, iVal-den[i]);
+    }
+    iNPik = maxPossibleWays(den, i-1, iVal);
+
+    return (iPik + iNPik);
+}
+
+long countWaysToMakeChange(int *denominations, int n, int value)
+{
+    return maxPossibleWays(denominations, n-1, value);
 }
