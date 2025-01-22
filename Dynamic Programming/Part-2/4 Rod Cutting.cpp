7a9723c2293e6d3f39018160ac4c248e6654953d
Vinayak Nikam <vinayaknikam2001@gmail.com>
2025-01-22 08:38:24 +0530

    Revising DP

--- Dynamic Programming/Part-2/4 Rod Cutting.cpp
+++ Dynamic Programming/Part-2/4 Rod Cutting.cpp
@@ -1,14 +1,35 @@
 // https://www.codingninjas.com/codestudio/problems/800284?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
 // Self Solved Wrong ans in sample test because silly mistake of (i-1 in func call...
 /*
+    //Mistake 1
 	nPik = func(i-1, rodSize, price, dp);
     if((i+1) <= rodSize)
         pik = price[i] + func(i-1, rodSize-(i+1), price, dp);
+
+
+    //Mistake 2 (During Revision)
+    In below line
+    pik = price[i] + func(i, (rodSize-i+1), price, dp);
+    Example:
+    Let a=10 and b=3:
+    1. (a - b + 1): 
+       ( a - b + 1 = 10 - 3 + 1 = 7 + 1 = 8)
+
+    2. a - (b + 1):
+       (a - (b + 1) = 10 - (3 + 1) = 10 - 4 = 6)
+
+    Conclusion:
+    The placement of parentheses changes the order of operations and, therefore, the result
+
 */
+
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
 typedef long long int lli;
+
+
 
 lli func(int i, int rodSize,  vector<int> &price, vector<vector<lli>> &dp)
 {
